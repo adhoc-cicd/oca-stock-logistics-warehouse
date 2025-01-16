mmequignon/stock-logistics-warehouse

[ADD] stock_location_zone

stock_location_zone/__manifest__.py
@@ -1,0 +1,24 @@
+# Copyright 2017 Syvain Van Hoof (Okia sprl) <sylvainvh@okia.be>
+# Copyright 2016-2019 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock Location Zone',
+    'version': '12.0.1.0.0',
+    'author': "BCIM, Okia, Odoo Community Association (OCA)",
+    'website': "https://github.com/OCA/stock-logistics-warehouse",
+    'summary': "Add coordinate attributes on stock location. "
+               "Define picking zone with links to picking type.",
+    'category': 'Stock Management',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/picking_zone.xml',
+        'views/picking_type.xml',
+        'views/stock_location.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}

adhoc-cicd/oca-stock-logistics-warehouse

stock_location_zone/__manifest__.py
@@ -1,0 +1,23 @@
+# Copyright 2017 Syvain Van Hoof (Okia sprl) <sylvainvh@okia.be>
+# Copyright 2016-2019 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock Location Zone',
+    'version': '12.0.1.0.0',
+    'author': "BCIM, Okia, Odoo Community Association (OCA)",
+    'website': "https://github.com/OCA/stock-logistics-warehouse",
+    'summary': "Add coordinate attributes on stock location. "
+               "Define picking zone with links to picking type.",
+    'category': 'Stock Management',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking_zone.xml',
+        'views/stock_location.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}