OCA/stock-logistics-warehouse

[FIX] fix negative quant in blocked location
When forcing an outgoing move and then moving it. Odoo create a
negative quant. This quant should not be in the locked location
because no quand will go in it and so the negative quant will stay
here for ever

stock_location_lockdown/models/stock_quant.py
@@ -9,18 +9,13 @@ from odoo.exceptions import UserError
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    @api.model
-    def quants_move(
-            self, quants, move, location_to, location_from=False, lot_id=False,
-            owner_id=False, src_package_id=False, dest_package_id=False,
-            entire_pack=False):
-        if (
-                location_to.usage == 'internal' and
-                location_to.block_stock_entrance):
-            raise UserError(_(
-                "The location '%s' is not configured to receive stock.")
-                % location_to.display_name)
-        return super(StockQuant, self).quants_move(
-            quants, move, location_to, location_from=location_from,
-            lot_id=lot_id, owner_id=owner_id, src_package_id=src_package_id,
-            dest_package_id=dest_package_id, entire_pack=entire_pack)
+    @api.constrains('location_id')
+    def _check_location_blocked(self):
+        for record in self:
+            if record.location_id.block_stock_entrance:
+                raise UserError(
+                    _('The location %s is blocked and can '
+                      'not be used for moving the product %s')
+                    % (record.location_id.name, record.product_id.name)
+                    )
+        return True

adhoc-cicd/oca-stock-logistics-warehouse

stock_location_lockdown/models/stock_quant.py
@@ -3,24 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, _
-from odoo.exceptions import UserError
+from odoo.exceptions import ValidationError
 
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    @api.model
-    def quants_move(
-            self, quants, move, location_to, location_from=False, lot_id=False,
-            owner_id=False, src_package_id=False, dest_package_id=False,
-            entire_pack=False):
-        if (
-                location_to.usage == 'internal' and
-                location_to.block_stock_entrance):
-            raise UserError(_(
-                "The location '%s' is not configured to receive stock.")
-                % location_to.display_name)
-        return super(StockQuant, self).quants_move(
-            quants, move, location_to, location_from=location_from,
-            lot_id=lot_id, owner_id=owner_id, src_package_id=src_package_id,
-            dest_package_id=dest_package_id, entire_pack=entire_pack)
+    @api.constrains('location_id')
+    def _check_location_blocked(self):
+        for record in self:
+            if record.location_id.block_stock_entrance:
+                raise ValidationError(
+                    _('The location %s is blocked and can '
+                      'not be used for moving the product %s')
+                    % (record.location_id.name, record.product_id.name)
+                    )
+        return True